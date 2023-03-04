1de748e09d20113c8b7e5b0f8fe118c2d184a02f
harishnv90 <126871860+harishnv90@users.noreply.github.com>
2023-03-05 00:43:06 +0530

    Update carSafety.test.js

--- test/carSafety.test.js
+++ test/carSafety.test.js
@@ -4,18 +4,14 @@ const carSafety = require("../pages/carSafety.page")
 describe ("Car safety test", function(){
     it ("Verify page title",async function(){
         browser.url('/intl/v/car-safety/a-million-more')
-        console.log("**************************"+await browser.getTitle())
         browser.maximizeWindow()
-        console.log("**************************"+await browser.getTitle())
         await carSafety.AcceptCookies()
-        console.log("**************************"+await browser.getTitle())
         assert.equal("A million more | Volvo Cars - International",await browser.getTitle(), "Browser title not matching") 
-        console.log("**************************"+await browser.getTitle())
     })
 
-//     it ("Verify presence of Menu link on the home page",async function(){
-//         assert.equal(true,await carSafety.ismenuExist(), "Menu link does not exist on the homepage")
-//     })
+    it ("Verify presence of Menu link on the home page",async function(){
+        assert.equal(true,await carSafety.ismenuExist(), "Menu link does not exist on the homepage")
+    })
 
 //     it ("Verify presence of various items under Menu",async function(){
 //         await carSafety.clickMenu()
