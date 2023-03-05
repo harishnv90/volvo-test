e49680b8a1c01b1cbc4cdbfda4fb695b848b8c34
harishnv90 <126871860+harishnv90@users.noreply.github.com>
2023-03-05 22:13:13 +0530

    Update wdio-docker.conf.js

--- wdio-docker.conf.js
+++ wdio-docker.conf.js
@@ -25,7 +25,7 @@ exports.config = {
     ],
     // Patterns to exclude.
     exclude: [
-        // 'path/to/excluded/files'
+        './test/**/imageComparision.test.js'
     ],
     //
     // ============
@@ -155,20 +155,7 @@ exports.config = {
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
     // commands. Instead, they hook themselves up into the test process.
-    services: ['image-comparison',
-        // The options
-        {
-            // Some options, see the docs for more
-            baselineFolder: join(process.cwd(), './tests/'),
-            formatImageName: '{tag}-{logName}-{width}x{height}',
-            screenshotPath: join(process.cwd(), '.tmp/'),
-            savePerInstance: true,
-            autoSaveBaseline: true,
-            blockOutStatusBar: true,
-            blockOutToolBar: true,
-            ignoreNothing: true  // Without this option, it errors out only when mismatch is above 1.23%(by default)
-            // ... more options
-        }],
+    services: [],
     
     // Framework you want to run your specs with.
     // The following are supported: Mocha, Jasmine, and Cucumber
