4ff67973c839ed0d9f7cf3c71923f3f818f9dc85
harishnv90 <126871860+harishnv90@users.noreply.github.com>
2023-03-05 21:21:20 +0530

    Update carSafety.test.js

--- test/carSafety.test.js
+++ test/carSafety.test.js
@@ -6,7 +6,7 @@ describe ("Car safety test", function(){
         browser.url('/intl/v/car-safety/a-million-more')
         browser.maximizeWindow()
         await carSafety.AcceptCookies()
-        assert.equal("A million more | Volvo Cars - International",await browser.getTitle(), "Browser title not matching") 
+        assert.equal("A million more | Volvo Cars - International",await browser.getTitle(), "Browser title doesn't match") 
     })
 
     it ("Verify presence of Menu link on the home page",async function(){
@@ -23,34 +23,8 @@ describe ("Car safety test", function(){
         assert.equal(true,await carSafety.isinternationalOptionExist(), "International item does not exist under Menu")
     })
 
-    // it ("Verify presence of cars span", function(){
-    //     assert.equal(true,carSafety.isCarsSpanExist(), "Car span not exist")         
-    // })
-
-    // it ("Verify presence of various types of cars", function(){
-    //     carSafety.clickCars()
-    //     assert.equal(true,carSafety.iselectricHeadingExist(), "Electric car type not displayed")
-    //     assert.equal(true,carSafety.ishybridsHeadingExist(), "Hybrid car type not displayed")
-    //     assert.equal(true,carSafety.ismildHybridsHeadingExist(), "Mild hybrid car type not displayed")         
-    // })
-
-    // it ("Verify description of each type of car", function(){
-    //     assert.equal(true,carSafety.ispureElectricDescriptionExist(), "Pure electric description not displayed")
-    //     assert.equal(true,carSafety.isplugInHybridsDescriptionExist(), "Plug in hybrids description not displayed")
-    //     assert.equal(true,carSafety.ismildHybridsMicroHybridsDescriptionExist(), "Mild hybrid micro hybrid description not displayed")
-    //     carSafety.clickClose()       
-    // })
-
-    // it ("Verify site navigation click and presence of options under it", function(){
-    //     carSafety.clicksiteNavigationIcon()
-    //     assert.equal(true,carSafety.isbuyOptionExist(), "Buy option does not exist")
-    //     assert.equal(true,carSafety.isownOptionExist(), "Own option does not exist")
-    //     assert.equal(true,carSafety.iswhyVolvoOptionExist(), "Why Volvo option does not exist")
-    //     assert.equal(true,carSafety.isexploreOptionExist(), "Explore option does not exist")
-    //     assert.equal(true,carSafety.ismoreOptionExist(), "More option does not exist")
-    //     assert.equal(true,carSafety.isinternationalOptionExist(), "International option does not exist")          
-    // })
-
-    
+    it ("Verify presence of heading 'Explore our models'",async function(){
+        assert.equal(true,await carSafety.isexploreOurModelsHeadingExist(), "'Explore our models' heading does not exists")
+    })
 
 })
